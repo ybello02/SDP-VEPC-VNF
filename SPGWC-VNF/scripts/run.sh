978bd491022136c41d652b9594f3cfe28b7e5561
ybello02 <56275192+ybello02@users.noreply.github.com>
2020-10-17 16:17:38 -0400

    Update run.sh

--- SPGWC-VNF/scripts/run.sh
+++ SPGWC-VNF/scripts/run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-echo $sdpNetwork
-service mysql start
-sudo node ./SDPController/sdpController.js 
+nohup tshark -i eth0 -i lo:p5c -i lo:s5c -w /tmp/spgwc_check_run.pcap 2>&1 > /dev/null
+nohup /openair-spgwc/bin/oai_spgwc -o -c /openair-spgwc/etc/spgw_c.conf > spgwc_check_run.log 2>&1
