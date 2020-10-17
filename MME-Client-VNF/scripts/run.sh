4d74813512139cb98e0b5d1a926472a0a3b1d1bf
ybello02 <56275192+ybello02@users.noreply.github.com>
2020-10-17 16:11:53 -0400

    Update run.sh

--- MME-Client-VNF/scripts/run.sh
+++ MME-Client-VNF/scripts/run.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 
-echo $sdpNetwork
-service mysql start
-sudo node ./SDPController/sdpController.js 
+nohup tshark -i eth0 -i eth1 -w /tmp/hss_check_run.pcap 2>&1 > /dev/null
+nohup /openair-mme/bin/oai_hss -j /openair-mme/etc/hss_rel14.json --reloadkey true > hss_check_run.log 2>&1
