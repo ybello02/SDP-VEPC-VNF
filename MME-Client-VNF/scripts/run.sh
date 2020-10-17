870811b67f3e4070e36f949766571bce07afdd60
ybello02 <56275192+ybello02@users.noreply.github.com>
2020-10-17 16:14:49 -0400

    Update run.sh

--- MME-Client-VNF/scripts/run.sh
+++ MME-Client-VNF/scripts/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-nohup tshark -i eth0 -i eth1 -w /tmp/hss_check_run.pcap 2>&1 > /dev/null
-nohup /openair-mme/bin/oai_hss -j /openair-mme/etc/hss_rel14.json --reloadkey true > hss_check_run.log 2>&1
+nohup tshark -i eth0 -i lo:s10 -w /tmp/mme_check_run.pcap 2>&1 > /dev/null
+nohup /openair-mme/bin/oai_mme -c /openair-mme/etc/mme.conf > mme_check_run.log 2>&1
+
+
