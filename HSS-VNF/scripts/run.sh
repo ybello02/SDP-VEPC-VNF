36ad054271f71ee41c1ebb3afa6e14a819481107
ybello02 <56275192+ybello02@users.noreply.github.com>
2020-10-17 16:14:09 -0400

    Update run.sh

--- HSS-VNF/scripts/run.sh
+++ HSS-VNF/scripts/run.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
 nohup tshark -i eth0 -i lo:s10 -w /tmp/mme_check_run.pcap 2>&1 > /dev/null
+nohup /openair-mme/bin/oai_mme -c /openair-mme/etc/mme.conf > mme_check_run.log 2>&1
