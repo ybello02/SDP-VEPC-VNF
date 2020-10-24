9725ae72e3536b2505697ee3c62d846a8ce4d1ac
leo104 <jsingh01@manhattan.edu>
2020-10-24 12:58:48 -0400

    latest

--- HSS-VNF/scripts/run.sh
+++ HSS-VNF/scripts/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-nohup tshark -i eth0 -i eth1 -w /tmp/hss_check_run.pcap 2>&1 > /dev/null
-nohup /openair-hss/bin/oai_hss -j /openair-mme/etc/hss_rel14.json --reloadkey true > hss_check_run.log 2>&1
+#nohup tshark -i eth0 -i eth1 -w /tmp/hss_check_run.pcap 2>&1 > /dev/null
+#nohup /openair-hss/bin/oai_hss -j /openair-mme/etc/hss_rel14.json --reloadkey true > hss_check_run.log 2>&1
+
+/bin/bash/ -c "sleep infinity"
